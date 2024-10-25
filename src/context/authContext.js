18ea85a29bf1046b74944f222899067466f1bd77
Josafat11 <116617414+Josafat11@users.noreply.github.com>
2024-10-25 01:00:15 -0600

    Cambios 5

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -13,13 +13,13 @@ export const AuthProvider = ({ children }) => {
 // Función para manejar el login
 const login = async (email, password) => {
   try {
-    const response = await fetch(`${CONFIGURACIONES.BASEURL}/auth/login`, {
+    const response = await fetch(`${CONFIGURACIONES.BASEURL}/auth/login`, { 
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ email, password }),
-      credentials: 'include', // Importante para enviar las cookies de sesión al servidor
+      credentials: 'include', 
     });
 
     const data = await response.json();
@@ -50,7 +50,7 @@ const checkSession = async () => {
       return;
     }
 
-    const response = await fetch(`${CONFIGURACIONES.BASEURL}/auth/check-session`, {
+    const response = await fetch(`${CONFIGURACIONES.BASEURL2}/auth/check-session`, {
       method: 'GET',
       credentials: 'include', // Para mantener la cookie de la sesión
     });
@@ -82,7 +82,7 @@ const checkSession = async () => {
 // Función para cerrar sesión
 const logout = async () => {
   try {
-    const response = await fetch(`${CONFIGURACIONES.BASEURL}/auth/logout`, {
+    const response = await fetch(`${CONFIGURACIONES.BASEURL2}/auth/logout`, {
       method: 'POST',
       credentials: 'include', // Importante para cerrar la sesión correctamente en el servidor
     });
